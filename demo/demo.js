26161cf1c2142aa779cfe08e5af53ba7a1568050
Kovacs Arthur <kovacs.arthur.kraid@gmail.com>
2016-09-12 16:04:47 +0300

    First trial with differential direction management. Needs testing.

--- demo/demo.js
+++ demo/demo.js
@@ -2,6 +2,8 @@ angular.module('app', ['ngAnimate','artJoystick']);
 
 angular.module('app').controller('DemoCtrl',function($scope,$http, $q, artJoystickExternal, $timeout){
 
-  
-
+    $scope.getWheelsSpeed = function(response) {
+        $scope.wLeft = response.left;
+        $scope.wRight = response.right;
+    };
 });
